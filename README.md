483e437bea0a89ffcf06a0fb50f715ee74aeebe4
mihalypodobni <71064763+mihalypodobni@users.noreply.github.com>
2021-01-19 00:29:31 +0100

    v7, added square step fill, simplified addClass and div creator

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# mihalypodobni.github.io
+# walk-through-raster-recode
+Practicing JS while trying to recreate Frieder Nake's Walk-Through-Raster Series
+
+[Project preview on github.io](http://mihalypodobni.github.io)
--- main.js
+++ main.js
@@ -1,180 +1,183 @@
-let size = 2500;
-let mergedSpiral = [].concat.apply([], createSpiral()); // merges the arrays of createSpiral
-let mergedLeftUp = [].concat.apply([], createLeftUp()); // merges the arrays of createSpiral
+let width=50;
+let arrayLength = width*width;
+let mergedLinear = [];
+mergedLinear = createLinearArray();
+let mergedSpiral = [].concat.apply([], createSpiralArray()); // merges the arrays of createSpiralArray
+let mergedupperLeft = [].concat.apply([], createUpperLeftArray()); // merges the arrays of createSpiralArray
+let mergedSquare = [].concat.apply([], createSquareArray());
 
-function createDivsLinear() {
-    for (let i = 0; i <= size-1; i++) {         //loop to cycle "size" times
-        //Linear Stroke
-        let div2 = document.createElement('div');  // create a new div element
-        div2.setAttribute("id", `linear`+(i+1));          // add id to the new div 
-        document.getElementById("linear").appendChild(div2);          // add the newly created element  to the DOM
-        //Linear Color
-        let div3 = document.createElement('div');  // create a new div element
-        div3.setAttribute("id", `linearCol`+(i+1));  // add id to the new div 
-        document.getElementById("linearCol").appendChild(div3); // add the newly created element  to the DOM
+
+//Arrays, fill directions and fill modes
+let allArray = [mergedLinear, mergedSpiral, mergedupperLeft, mergedSquare];
+let rasterTypes = [`linear`, `spiral`,`upperLeft`,`square`];
+let fillMode = [`Col`,`Stroke`];
+
+//Create divs in DOM for each raster
+function createAllDivs(){
+    for (let types = 0; types < rasterTypes.length; types++) {
+        for (let fill = 0; fill < fillMode.length; fill++) {
+            for (let idCount = 0; idCount < arrayLength; idCount++) {
+                div = document.createElement('div');
+                div.setAttribute("id", rasterTypes[types]+fillMode[fill]+parseInt(allArray[types][idCount]));
+                document.getElementById(rasterTypes[types]+fillMode[fill]).appendChild(div); 
+            }            
+        }
     }
 }
 
-function createDivsSpiral() {
-    for (let i = 0; i <= size-1; i++) {           //loop to cycle "size" times
-        //spiral Strokes
-        let div2= document.createElement('div');    // create a new div element
-        div2.setAttribute("id", `spiral`+mergedSpiral[i]);  // add id to the new div 
-        document.getElementById("spiral").appendChild(div2);          // add the newly created element  to the DOM
-        //spiralColor
-        let div3 = document.createElement('div');  // create a new div element
-        div3.setAttribute("id", `spiralCol`+mergedSpiral[i]);  // add id to the new div 
-        document.getElementById("spiralCol").appendChild(div3); // add the newly created element to the DOM
-    }
-}
-
-
-function createDivsLeftUp() {
-    for (let i = 0; i <= size-1; i++) {           //loop to cycle "size" times
-        //spiral Strokes
-        let div2= document.createElement('div');    // create a new div element
-        div2.setAttribute("id", `leftUp`+mergedLeftUp[i]);  // add id to the new div 
-        document.getElementById("leftUp").appendChild(div2);          // add the newly created element  to the DOM
-        //spiralColor
-        let div3 = document.createElement('div');  // create a new div element
-        div3.setAttribute("id", `leftUpCol`+mergedLeftUp[i]);  // add id to the new div 
-        document.getElementById("leftUpCol").appendChild(div3); // add the newly created element to the DOM
-    }
-}
-
+//Fill the divs with random stroke / bg color
 function addClass() {
     let stroke;
     let bgColor;
     let offset = -2;                        // to offsetts the probalility over time
-    for (let i = 0; i <= size-1; i++) {       //cycles through all the divs
+    for (let i = 1; i <= arrayLength; i++) {       //cycles through all the divs
         let randomNumber = (Math.random() * 3);    //generates random number between 0-2.99
-
         //_____________________________________ possible cases
         if (randomNumber>=(2-offset)) {
             stroke = `empty`;
             bgColor = "white";                    
         } else if (randomNumber>1-(offset/8) && randomNumber<=2-(offset/8)) {
-            stroke = `bottom`;
+            stroke = `horizontal`;
             bgColor = "darkgrey";                    
         } else if (randomNumber<=1){                      
-            stroke = `right`;
+            stroke = `vertical`;
             bgColor = "black";                    
         }
-        //_____________________________________ possible cases
 
         offset += 0.00125;                       // raises the probalility of empty over time
 
-        document.getElementById((`leftUpCol`+(i+1))).className ="";  //resets class of to the div it parses
-        document.getElementById((`leftUpCol`+(i+1))).style.backgroundColor =`${bgColor}`; //adds greyscale background color
-        document.getElementById((`spiralCol`+(i+1))).className ="";  //resets class of to the div it parses
-        document.getElementById((`spiralCol`+(i+1))).style.backgroundColor =`${bgColor}`; //adds greyscale background color
-        document.getElementById((`linearCol`+(i+1))).className ="";  //resets class of to the div it parses
-        document.getElementById((`linearCol`+(i+1))).style.backgroundColor =`${bgColor}`;  //adds greyscale background color
-        document.getElementById((`linear`+(i+1))).className ="";  //resets class of to the div it parses
-        document.getElementById((`linear`+(i+1))).classList.add(`${stroke}`); // adds the generated vertical, horizontal or null stroke
-        document.getElementById((`spiral`+(i+1))).className ="";  //resets class of to the div it parses
-        document.getElementById((`spiral`+(i+1))).classList.add(`${stroke}`); // adds the generated vertical, horizontal or null stroke
-        document.getElementById((`leftUp`+(i+1))).className ="";  //resets class of to the div it parses
-        document.getElementById((`leftUp`+(i+1))).classList.add(`${stroke}`); // adds the generated vertical, horizontal or null stroke
+        let rasterTypes = [`linear`, `spiral`,`upperLeft`,`square`];
+        let fillMode = [`Col`,`Stroke`];
+
+        for (let j = 0; j < rasterTypes.length; j++) {
+            document.getElementById((rasterTypes[j]+`Stroke`+(i))).className ="";  //resets class of to the div it parses
+            document.getElementById((rasterTypes[j]+`Stroke`+(i))).classList.add(`${stroke}`);
+            document.getElementById((rasterTypes[j]+`Col`+(i))).className ="";  //resets class of to the div it parses
+            document.getElementById((rasterTypes[j]+`Col`+(i))).style.backgroundColor =`${bgColor}`;                
+        }
+    }
+}
+
+//Create arrays with different fill directions
+    function createLinearArray() {
+        for (let i = 0; i < arrayLength; i++) {
+            mergedLinear.push(i+1);
+        }
+        return mergedLinear;
+    }
+
+    function createSpiralArray(width){
+        width = 50;
+        let spiralArray = [];
         
-    }
-}
-
-function createSpiral(size){
-    size = 50;
-    let spiralArray = [];
-    
-    for (let i = 0; i < size; i++) {
-        spiralArray[i] = Array(size)
-    }
-
-    let topEdge = 0;
-    let bottomEdge = (size-1);
-    let leftEdge = 0;
-    let rightEdge = (size-1);
-    let counter = 1;
-    let direction = 'LEFT';
-    let x = 0;
-    let y = 0;
-
-    while (topEdge <= bottomEdge && leftEdge <= rightEdge) {
-        if (direction === "LEFT" && x <= rightEdge) {
-            spiralArray[y][x++] = counter++;
-        } else if (direction === "LEFT" && x > rightEdge) {
-            direction = "DOWN";
-            x = rightEdge;
-            y = ++topEdge;
-        } else if (direction === 'DOWN' && y <= bottomEdge) {
-            spiralArray[y++][x] = counter++;
-        } else if (direction === 'DOWN' && y > bottomEdge) {
-            direction = 'RIGHT';
-            x = --rightEdge;
-            y = bottomEdge;
-        } else if (direction === 'RIGHT' && x >= leftEdge) {
-            spiralArray[y][x--] = counter++;
-        } else if (direction === 'RIGHT' && x < leftEdge) {
-            direction = 'UP';
-            x = leftEdge;
-            y = --bottomEdge;
-        }  else if (direction === 'UP' && y >= topEdge) {
-            spiralArray[y--][x] = counter++;
-        } else if (direction === 'UP' && y < topEdge) {
-            direction = 'LEFT';
-            x = ++leftEdge;
-            y = topEdge;
+        for (let i = 0; i < width; i++) {
+            spiralArray[i] = Array(width);
         }
-    }
-    return spiralArray;
-}
 
-function createLeftUp() {
+        let topEdge = 0;
+        let bottomEdge = (width-1);
+        let leftEdge = 0;
+        let rightEdge = (width-1);
+        let counter = 1;
+        let direction = 'LEFT';
+        let x = 0;
+        let y = 0;
 
-    var shiftCounter = [];
-    var leftUpArray =[];
-    let counter=1;
-    let size= 50;
-    
-    //create emtpy array
-    for (let y = 0; y < size; y++) {
-        leftUpArray [y] = new Array(size);
-    }
-    //------------------
-    
-
-    
-    //create counter utility array
-    for (var s = 1; s <= size; s++) {
-        shiftCounter.push(s);
-    }
-    for (var s = size; s >= 1; s--) {
-        shiftCounter.push(s);
-    }
-    //----------------------------
-
-    //set first cell value
-    leftUpArray[0][0]=1;
-    //------------------
-    
-    //fill in first colunm values
-    for (let y = 1; y < size; y++) {
-        leftUpArray[y][0]= parseInt(leftUpArray[y-1][0])+counter;
-        ++counter;
-    }
-    counter = 0;
-    //----------------------------
-    
-    
-    //fill in cell values
-    for (let y = 0; y < size; y++) {
-        for (let x = 1; x < size; x++) {
-            leftUpArray[y][x]= parseInt(leftUpArray[y][x-1])+shiftCounter[x+y];
-            };
+        while (topEdge <= bottomEdge && leftEdge <= rightEdge) {
+            if (direction === "LEFT" && x <= rightEdge) {
+                spiralArray[y][x++] = counter++;
+            } else if (direction === "LEFT" && x > rightEdge) {
+                direction = "DOWN";
+                x = rightEdge;
+                y = ++topEdge;
+            } else if (direction === 'DOWN' && y <= bottomEdge) {
+                spiralArray[y++][x] = counter++;
+            } else if (direction === 'DOWN' && y > bottomEdge) {
+                direction = 'RIGHT';
+                x = --rightEdge;
+                y = bottomEdge;
+            } else if (direction === 'RIGHT' && x >= leftEdge) {
+                spiralArray[y][x--] = counter++;
+            } else if (direction === 'RIGHT' && x < leftEdge) {
+                direction = 'UP';
+                x = leftEdge;
+                y = --bottomEdge;
+            }  else if (direction === 'UP' && y >= topEdge) {
+                spiralArray[y--][x] = counter++;
+            } else if (direction === 'UP' && y < topEdge) {
+                direction = 'LEFT';
+                x = ++leftEdge;
+                y = topEdge;
+            }
         }
-    //----------------------------
-    return leftUpArray;
+        return spiralArray;
     }
 
-createDivsLeftUp();
-createDivsLinear();
-createDivsSpiral();
+    function createUpperLeftArray() {
+        var shiftCounter = [];
+        var leftUpArray =[];
+        let counter=1;
+        let width = 50;
+        //create emtpy array
+        for (let y = 0; y < width; y++) {
+            leftUpArray [y] = new Array(width);
+        }
+        //create counter utility array
+        for (var s = 1; s <= width; s++) {
+            shiftCounter.push(s);
+        }
+        for (var s = width; s >= 1; s--) {
+            shiftCounter.push(s);
+        }
+        //set starting cell value
+        leftUpArray[0][0]=1;
+        //fill in first colunm values
+        for (let y = 1; y < width; y++) {
+            leftUpArray[y][0]= parseInt(leftUpArray[y-1][0])+counter;
+            ++counter;
+        }
+        counter = 0;
+        //fill in cell values
+        for (let y = 0; y < width; y++) {
+            for (let x = 1; x < width; x++) {
+                leftUpArray[y][x]= parseInt(leftUpArray[y][x-1])+shiftCounter[x+y];
+                };
+            }
+        return leftUpArray;
+    }
+
+    function createSquareArray(width) {
+        width = 50;
+        var leftUpArray =[];
+        let shiftCounter = [];
+    
+        //create counter utility
+        for (var i = 1; i <= width; ++i) {
+            shiftCounter.push((i*2)-1);
+        }
+    
+        //create emtpy array
+        for (let y = 0; y < width; y++) {
+            leftUpArray [y] = new Array(width);
+        }
+    
+        //set starting cell value
+        leftUpArray[0][0]=1;
+        //fill in first colunm values
+        for (let y = 1; y < width; y++) {
+            leftUpArray[y][0]= parseInt(leftUpArray[y-1][0])+parseInt(shiftCounter[y-1]);
+        }
+        counter = 2;
+        //fill in cell values row by row
+        for (let y = 0; y < width; y++) {
+            for (let x = 1; x < width; x++) {
+                leftUpArray[y][x]= parseInt(leftUpArray[y][x-1])+parseInt(shiftCounter[x]);
+                };
+            shiftCounter.splice(y+1,1,1)
+            }
+            
+        return leftUpArray;
+    }
+
+createAllDivs();
 addClass();
