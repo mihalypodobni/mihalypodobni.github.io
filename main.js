a40d3fa0c764d57cead91f9f2441f8f727bf5729
mihalypodobni <71064763+mihalypodobni@users.noreply.github.com>
2021-01-18 15:46:03 +0100

    v6 with upper left raster fill
    Added new raster fill mode, and removed number array displays

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 let size = 2500;
-let merged = [].concat.apply([], createSpiral()); // merges the arrays of createSpiral
+let mergedSpiral = [].concat.apply([], createSpiral()); // merges the arrays of createSpiral
+let mergedLeftUp = [].concat.apply([], createLeftUp()); // merges the arrays of createSpiral
 
 function createDivsLinear() {
     for (let i = 0; i <= size-1; i++) {         //loop to cycle "size" times
-
-        //Linear Numbers
-        let div1 = document.createElement('div');  // create a new div element
-        div1.setAttribute("id", `linearNum`+(i+1));  // add id to the new div 
-        let DivNumber = document.createTextNode(`${i+1}`);  // declare number to be put in the div
-        div1.appendChild(DivNumber); // add numbers to div
-        document.getElementById("linearNum").appendChild(div1); // add the newly created element  to the DOM
         //Linear Stroke
         let div2 = document.createElement('div');  // create a new div element
         div2.setAttribute("id", `linear`+(i+1));          // add id to the new div 
@@ -23,20 +17,28 @@ function createDivsLinear() {
 
 function createDivsSpiral() {
     for (let i = 0; i <= size-1; i++) {           //loop to cycle "size" times
-        //spiralNumbers
-        let div1= document.createElement('div');    // create a new div element
-        div1.setAttribute("id", `spiralNum`+ merged[i]);          // add id to the new div
-        let DivNumber = document.createTextNode(merged[i]); 
-        div1.appendChild(DivNumber);
-        document.getElementById("spiralNum").appendChild(div1);          // add the newly created element
         //spiral Strokes
         let div2= document.createElement('div');    // create a new div element
-        div2.setAttribute("id", `spiral`+merged[i]);  // add id to the new div 
+        div2.setAttribute("id", `spiral`+mergedSpiral[i]);  // add id to the new div 
         document.getElementById("spiral").appendChild(div2);          // add the newly created element  to the DOM
         //spiralColor
         let div3 = document.createElement('div');  // create a new div element
-        div3.setAttribute("id", `spiralCol`+merged[i]);  // add id to the new div 
+        div3.setAttribute("id", `spiralCol`+mergedSpiral[i]);  // add id to the new div 
         document.getElementById("spiralCol").appendChild(div3); // add the newly created element to the DOM
+    }
+}
+
+
+function createDivsLeftUp() {
+    for (let i = 0; i <= size-1; i++) {           //loop to cycle "size" times
+        //spiral Strokes
+        let div2= document.createElement('div');    // create a new div element
+        div2.setAttribute("id", `leftUp`+mergedLeftUp[i]);  // add id to the new div 
+        document.getElementById("leftUp").appendChild(div2);          // add the newly created element  to the DOM
+        //spiralColor
+        let div3 = document.createElement('div');  // create a new div element
+        div3.setAttribute("id", `leftUpCol`+mergedLeftUp[i]);  // add id to the new div 
+        document.getElementById("leftUpCol").appendChild(div3); // add the newly created element to the DOM
     }
 }
 
@@ -51,7 +53,7 @@ function addClass() {
         if (randomNumber>=(2-offset)) {
             stroke = `empty`;
             bgColor = "white";                    
-        } else if (randomNumber>1-(offset/4) && randomNumber<=2-(offset/4)) {
+        } else if (randomNumber>1-(offset/8) && randomNumber<=2-(offset/8)) {
             stroke = `bottom`;
             bgColor = "darkgrey";                    
         } else if (randomNumber<=1){                      
@@ -62,6 +64,8 @@ function addClass() {
 
         offset += 0.00125;                       // raises the probalility of empty over time
 
+        document.getElementById((`leftUpCol`+(i+1))).className ="";  //resets class of to the div it parses
+        document.getElementById((`leftUpCol`+(i+1))).style.backgroundColor =`${bgColor}`; //adds greyscale background color
         document.getElementById((`spiralCol`+(i+1))).className ="";  //resets class of to the div it parses
         document.getElementById((`spiralCol`+(i+1))).style.backgroundColor =`${bgColor}`; //adds greyscale background color
         document.getElementById((`linearCol`+(i+1))).className ="";  //resets class of to the div it parses
@@ -70,6 +74,9 @@ function addClass() {
         document.getElementById((`linear`+(i+1))).classList.add(`${stroke}`); // adds the generated vertical, horizontal or null stroke
         document.getElementById((`spiral`+(i+1))).className ="";  //resets class of to the div it parses
         document.getElementById((`spiral`+(i+1))).classList.add(`${stroke}`); // adds the generated vertical, horizontal or null stroke
+        document.getElementById((`leftUp`+(i+1))).className ="";  //resets class of to the div it parses
+        document.getElementById((`leftUp`+(i+1))).classList.add(`${stroke}`); // adds the generated vertical, horizontal or null stroke
+        
     }
 }
 
@@ -120,7 +127,54 @@ function createSpiral(size){
     return spiralArray;
 }
 
+function createLeftUp() {
 
+    var shiftCounter = [];
+    var leftUpArray =[];
+    let counter=1;
+    let size= 50;
+    
+    //create emtpy array
+    for (let y = 0; y < size; y++) {
+        leftUpArray [y] = new Array(size);
+    }
+    //------------------
+    
+
+    
+    //create counter utility array
+    for (var s = 1; s <= size; s++) {
+        shiftCounter.push(s);
+    }
+    for (var s = size; s >= 1; s--) {
+        shiftCounter.push(s);
+    }
+    //----------------------------
+
+    //set first cell value
+    leftUpArray[0][0]=1;
+    //------------------
+    
+    //fill in first colunm values
+    for (let y = 1; y < size; y++) {
+        leftUpArray[y][0]= parseInt(leftUpArray[y-1][0])+counter;
+        ++counter;
+    }
+    counter = 0;
+    //----------------------------
+    
+    
+    //fill in cell values
+    for (let y = 0; y < size; y++) {
+        for (let x = 1; x < size; x++) {
+            leftUpArray[y][x]= parseInt(leftUpArray[y][x-1])+shiftCounter[x+y];
+            };
+        }
+    //----------------------------
+    return leftUpArray;
+    }
+
+createDivsLeftUp();
 createDivsLinear();
 createDivsSpiral();
 addClass();
